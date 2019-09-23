3282091a4bb45f553e8e5f79325c961d9b673d66
GEDEON NZYANGA <gedeonnzyanga@gmail.com>
2019-09-23 11:21:10 +0200

    creation library emprunts

---
+++ Emprunts_Lib/Emprunt.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Connexion_manager;
+using Parametre;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emprunts_Lib
+{
+    class Emprunt : IEmprunt
+    {
+        public int Id { get; set; }
+        public float Montant { get; set; }
+        public float MontantRemb { get; set; }
+        public int Reftier { get; set; }
+        public DateTime DateEmprunt { get; set; }
+        public DateTime DateRembu { get; set; }
+        public string Tier { get; set; }
+
+        public int Nouveau()
+        {
+            if (ImplementeConnexion.Instance.Conn.State == ConnectionState.Closed)
+                ImplementeConnexion.Instance.Conn.Open();
+            using (IDbCommand cmd = ImplementeConnexion.Instance.Conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT MAX(code_emprunt) AS last_id FROM emprunts";
+                IDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    if (dr["last_id"] == DBNull.Value)
+                        Id = 1;
+                    else
+                        Id = Convert.ToInt32(dr["last_id"].ToString());
+                }
+                dr.Dispose();
+                cmd.Dispose();
+            }
+            return Id;
+        }
+
+        public void Save(IEmprunt emprunt)
+        {
+            if (ImplementeConnexion.Instance.Conn.State == ConnectionState.Closed)
+                ImplementeConnexion.Instance.Conn.Open();
+            using (IDbCommand cmd = ImplementeConnexion.Instance.Conn.CreateCommand())
+            {
+                cmd.CommandText = "INSERT_EMPRUNT";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(Parametres.Instance.AjouterParametre(cmd, "@code", 5, DbType.Int32, Id));
+                cmd.Parameters.Add(Parametres.Instance.AjouterParametre(cmd, "@montant", 10, DbType.Double, Montant));
+                cmd.Parameters.Add(Parametres.Instance.AjouterParametre(cmd, "@date_remb", 20, DbType.Date, DateRembu));
+                cmd.Parameters.Add(Parametres.Instance.AjouterParametre(cmd, "@montant_remb", 20, DbType.Double, MontantRemb));
+                cmd.Parameters.Add(Parametres.Instance.AjouterParametre(cmd, "@reftier", 20, DbType.Int32, Reftier));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Saved Successfully !!!", "Message...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cmd.Dispose();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            if (ImplementeConnexion.Instance.Conn.State == ConnectionState.Closed)
+                ImplementeConnexion.Instance.Conn.Open();
+            using (IDbCommand cmd = ImplementeConnexion.Instance.Conn.CreateCommand())
+            {
+                cmd.CommandText = "";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(Parametres.Instance.AjouterParametre(cmd, "", 5, DbType.Int32, id));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Deleted Successfully !!!", "Message...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cmd.Dispose();
+            }
+        }
+
+        public IEmprunt GetEmprunt (IDataReader dr)
+        {
+            IEmprunt emprunt = new Emprunt();
+            emprunt.Id = Convert.ToInt32(dr[""].ToString());
+            emprunt.Tier = dr[""].ToString();
+            emprunt.Montant = float.Parse(dr[""].ToString());
+            emprunt.MontantRemb = float.Parse(dr[""].ToString());
+            emprunt.DateEmprunt = DateTime.Parse(dr[""].ToString());
+            emprunt.DateRembu = DateTime.Parse(dr[""].ToString());
+            return emprunt;
+        }
+
+        public List<IEmprunt> AllEmprunt()
+        {
+            List<IEmprunt> list = new List<IEmprunt>();
+            if (ImplementeConnexion.Instance.Conn.State == ConnectionState.Closed)
+                ImplementeConnexion.Instance.Conn.Open();
+            using (IDbCommand cmd = ImplementeConnexion.Instance.Conn.CreateCommand())
+            {
+                cmd.CommandText = "";
+                cmd.CommandType = CommandType.StoredProcedure;
+                IDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    list.Add(GetEmprunt(dr));
+                }
+                cmd.Dispose();
+                dr.Close();
+            }
+            return list;
+        }
+
+        public IEmprunt OneEmprunt(int id)
+        {
+            IEmprunt tier = new Emprunt();
+            if (ImplementeConnexion.Instance.Conn.State == ConnectionState.Closed)
+                ImplementeConnexion.Instance.Conn.Open();
+            using (IDbCommand cmd = ImplementeConnexion.Instance.Conn.CreateCommand())
+            {
+                cmd.CommandText = "";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(Parametres.Instance.AjouterParametre(cmd, "code_tier", 5, DbType.Int32, Id));
+                IDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    tier = GetEmprunt(dr); 
+                }
+                cmd.Dispose();
+                dr.Dispose();
+            }
+            return tier;
+        }
+
+        public List<IEmprunt> Search(string recherche)
+        {
+            List<IEmprunt> list = new List<IEmprunt>();
+            if (ImplementeConnexion.Instance.Conn.State == ConnectionState.Closed)
+                ImplementeConnexion.Instance.Conn.Open();
+            using (IDbCommand cmd = ImplementeConnexion.Instance.Conn.CreateCommand())
+            {
+                cmd.CommandText = "";
+                cmd.CommandType = CommandType.StoredProcedure;
+                IDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    list.Add(GetEmprunt(dr));
+                }
+                cmd.Dispose();
+                dr.Close();
+            }
+            return list;
+        }
+    }
+}
---
+++ Emprunts_Lib/IEmprunt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emprunts_Lib
+{
+    public interface IEmprunt
+
+    {
+        int Id { get; set; }
+        float Montant { get; set; }
+        float MontantRemb { get; set; }
+        int Reftier { get; set; }
+        DateTime DateEmprunt { get; set; }
+        DateTime DateRembu { get; set; }
+        string Tier { get; set; }
+
+        int Nouveau();
+        void Save(IEmprunt emprunt);
+        void Delete(int id);
+        List<IEmprunt> AllEmprunt();
+        IEmprunt OneEmprunt(int id);
+    }
+}
